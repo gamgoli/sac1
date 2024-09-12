912be7ee494d68cc9a1b83dbaee36671b8627173
gamgoli <gamgoli@gmail.com>
2024-09-12 14:21:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+var getScriptPromisify = (src) => {
+  return new Promise((resolve) => {
+    $.getScript(src, resolve)
+  })
+}
+
+var parseMetadata = metadata => {
+  const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
+  const dimensions = []
+  for (const key in dimensionsMap) {
+    const dimension = dimensionsMap[key]
+    dimensions.push({ key, ...dimension })
+  }
+  const measures = []
+  for (const key in measuresMap) {
+    const measure = measuresMap[key]
+    measures.push({ key, ...measure })
+  }
+  return { dimensions, measures, dimensionsMap, measuresMap }
+}
+
 (function () {
 const template = document.createElement('template')
 template.innerHTML = `
@@ -8,34 +29,72 @@ Hello Web Component
 </div>
 `
 class Main extends HTMLElement {
-	constructor () {
-		super()
-		this._shadowRoot = this.attachShadow({ mode: 'open' })
-		this._shadowRoot.appendChild(template.content.cloneNode(true))
-		this._root = this._shadowRoot.getElementById('root')
-	}
-	onCustomWidgetResize (width, height){
-		this.render();
-	}
+constructor () {
+super()
 
-	onCustomWidgetAfterUpdate(changedProps){
-			//
-	}
+this._shadowRoot = this.attachShadow({ mode: 'open' })
+this._shadowRoot.appendChild(template.content.cloneNode(true))
 
-	onCustomWidgetDestroy(){
-			//
-	}
+this._root = this._shadowRoot.getElementById('root')
 
-	async render(){
-		//this._root.textContent = `Hello Custom Widget clientWidth: ${this.clientWidth}, clientHeight: ${this.clientHeight}`
-		const dataBinding = this.dataBinding
-		if(!dataBinding || dataBinding.state != 'success'){
-			return
-		}
-
-		this._root.textContent = JSON.stringify(dataBinding)
-	}
+this._eChart = null
 }
 
-customElements.define('com-sap-sac-exercise-hoseong003-main', Main)
+onCustomWidgetResize (width, height){
+this.render()
+}
+
+onCustomWidgetAfterUpdate(changedProps){
+//
+}
+
+onCustomWidgetDestroy(){
+//
+}
+
+async render(){
+const dataBinding = this.dataBinding
+if (!dataBinding || dataBinding.state !== 'success') { return }
+
+await getScriptPromisify('https://cdn.jsdelivr.net/npm/echarts@5.5.1/dist/echarts.min.js');
+
+
+
+const {data, metadata } = dataBinding
+const {dimensions, measureis} = parseMetadata(metadata)
+
+const categoryData = []
+// measures
+const series = measures.map(measure =>{
+return {
+id: measure.id,
+name: measure.label,
+data:[],
+key: measure.key,
+type: 'line',
+smooth: true
+}
+})
+data.forEach(row => {
+categoryData.push(dimensions.map(dimension => {
+return row[dimension.key].label
+}).join('/')) // dimension
+series.forEach(series => {
+series.data.push(row[series.key].raw)
+}) // measures
+})
+
+if (this._eChart) { echarts.dispose(this._eChart) }
+const eChart = this._eChart = echarts.init(this._root, 'main')
+const option = {
+xAxis: { type: 'category', data: categoryData },
+yAxis: { type: 'value' },
+tooltip: { trigger: 'axis' },
+series
+}
+eChart.setOption(option)
+}
+}
+
+customElements.define('com-sap-sac-exercise-hoseong004-main', Main)
 })()
