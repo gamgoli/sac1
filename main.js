582c8e152588b39b90d7cde423a34f2b0b361b47
gamgoli <gamgoli@gmail.com>
2024-09-12 13:13:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 (function () {
-const template = document.createElement('template')
-template.innerHTML = `
-<style>
-</style>
-<div id="root" style="width: 100%; height: 100%;">
-Hello WebComponent
-</div>
-`
-class Main extends HTMLElement {
-constructor () {
-super()
-this._shadowRoot = this.attachShadow({ mode: 'open' })
-this._shadowRoot.appendChild(template.content.cloneNode(true))
-this._root = this._shadowRoot.getElementById('root')
-}
+	const template = document.createElement('template')
+		template.innerHTML = `
+		<style>
+		</style>
+		<div id="root" style="width: 100%; height: 100%;">
+		Hello WebComponent
+		</div>
+	`
+	class Main extends HTMLElement {
+		constructor () {
+			super()
+			this._shadowRoot = this.attachShadow({ mode: 'open' })
+			this._shadowRoot.appendChild(template.content.cloneNode(true))
+			this._root = this._shadowRoot.getElementById('root')
+		}
 
 		onCustomWidgetResize(width, height){
 			this.render()
@@ -26,8 +26,8 @@ this._root = this._shadowRoot.getElementById('root')
 		}
 
 		render(){
-			this._root.textContext = 'Hello Custom Widget ClientWidth : ${this.clientWidth}, ClientHeight : ${this.clientHeight}'
+			this._root.textContext = `Hello Custom Widget ClientWidth : ${this.clientWidth}, ClientHeight : ${this.clientHeight}`
 		}
-}
-customElements.define('com-sap-sac-exercise-hoseong002-main', Main)
+	}
+	customElements.define('com-sap-sac-exercise-hoseong002-main', Main)
 })()
