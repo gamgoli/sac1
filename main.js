1746465a1835e41248ddc0a31a2147fbf0a6c751
gamgoli <gamgoli@gmail.com>
2024-09-12 11:15:04 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+(function(){
+	const template = document.createElement('tmplate')
+	template.innerHTML = '
+		<style>
+		</style>
+		<div id="root" style="width: 100%; height: 100%;">
+		Hello Custom Wijet - hosoeng.hwang
+		</div>
+		'
+	
+	class Main extends HTMLElement {
+		constructor () {
+			super()
+
+			this._shadowRoot = this.attachShadow({mode: 'open')
+			this._shadowRoot.appendChild(template.content.cloneNode(true))
+
+			this._root = this._shadowRoot.getElementById('root')
+		}
+	}
+
+	customElements.define('com-sap-sac-exercise-001-hoseong-main', Main)
+})()
